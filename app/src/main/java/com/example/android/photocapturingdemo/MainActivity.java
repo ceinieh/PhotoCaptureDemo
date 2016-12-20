f91fac83f8e0847ea65726a371ba032e6476d611
Chaker <ceinieh@hotmail.com>
2016-12-19 20:32:28 -0500

    Add two launchers and grid layout

--- app/src/main/java/com/example/android/photocapturingdemo/MainActivity.java
+++ app/src/main/java/com/example/android/photocapturingdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.photocapturingdemo;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
@@ -9,6 +10,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,16 +21,20 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
     private Button captureBtn;
     private ImageView imageView;
+    private boolean sentNotificationFlag = false;
     private static final int CAMERA_REQUEST = 1000;
-
-
+    int mNotificationId = 001;
+    private NotificationCompat.Builder mBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         captureBtn = (Button) findViewById(R.id.a_Take_Picture_btn);
         imageView = (ImageView) findViewById(R.id.a_ImageViewer);
-
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.notification_icon);
+        mBuilder.setContentTitle("Notification Alert, Click Me!");
+        mBuilder.setContentText("Hi, going to the background!");
 
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +59,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sentNotificationFlag = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sentNotificationFlag = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(sentNotificationFlag){
+            sendNotification();
+        }
+    }
+    private void sendNotification(){
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
 
 }
---
+++ app/src/main/java/com/example/android/photocapturingdemo/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.photocapturingdemo;
+
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class SecondActivity extends AppCompatActivity {
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+ //   public static TypedArray [] prgmImages={R.drawable.image1,R.drawable.image2,R.drawable.image3,R.drawable.image4,R.drawable.image5,R.drawable.image6};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+   }
+}
